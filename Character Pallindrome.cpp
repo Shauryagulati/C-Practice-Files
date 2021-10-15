b36f2971eed0daae0177f6826168da2f791e9bd6
Shaurya Gulati <Sgulati1020@gmail.com>
2021-10-15 22:13:06 +0530

    String

---
+++ Character Pallindrome.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    char arr[n+1];
+    cin>>arr;
+    bool check = 1;
+    
+    for(int i=0; i<n; i++){
+        if(arr[i] != arr[n-i-1]){
+            check = 0;
+            break;
+        }
+    }
+    if(check) 
+        cout<<"It is a Pallindrome";
+    else
+        cout<<"It is not a Pallindrome";
+    
+    return 0;
+}
